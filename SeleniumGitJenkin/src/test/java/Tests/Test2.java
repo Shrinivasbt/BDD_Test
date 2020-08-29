5c8f0801a325c8d4847a1cbfe0b3d7ee784d9a5a
Windows <Windows@Shri-PC>
2020-08-29 22:15:46 +0530

    First respo

---
+++ SeleniumGitJenkin/src/test/java/Tests/Test2.java
@@ -0,0 +1,13 @@
+package Tests;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+	@Test
+	public void print() {
+		
+		System.out.println("This is selenium demo 2");
+	}
+
+}
---
+++ SeleniumGitJenkin/src/test/java/Tests/Test3.java
@@ -0,0 +1,13 @@
+package Tests;
+
+import org.testng.annotations.Test;
+
+public class Test3 {
+
+	@Test
+	public void print() {
+		
+		System.out.println("This is selenium demo 3");
+	}
+
+}
